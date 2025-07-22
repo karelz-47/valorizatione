776ab209ba6c3491a91e55039cb38039a31fab45
karelz-47 <karel.zvolsky@gmail.com>
2025-07-22 11:08:47 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -493,7 +493,7 @@ def build_doc(
 # -------------------------------------------------------------------------
 
 def main():
-    st.set_page_config(page_title="📄 Generatore Lettera Valorizzazione",
+   st.set_page_config(page_title="📄 Generatore Lettera Valorizzazione",
                    layout="wide",  # more horizontal breathing room
                    initial_sidebar_state="expanded")    
    st.markdown(
